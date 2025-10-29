9768ae9564eff3dc43375cd66d156a37c48b6483
sadiecoffield <sadiec05@yahoo.com>
2025-10-29 12:33:00 +0000

    Change tabs to native tabs

--- src/app/(tabs)/_layout.js
+++ src/app/(tabs)/_layout.js
@@ -1,9 +1,9 @@
 import FontAwesome from "@expo/vector-icons/FontAwesome";
-import { Tabs } from "expo-router";
+import { NativeTabs, Label, Icon } from 'expo-router/unstable-native-tabs';
 
 export default function TabsLayout() {
   return (
-    <Tabs
+    <NativeTabs
       screenOptions={{
         headerShown: false,
         tabBarActiveTintColor: "#9b5de5",
@@ -12,24 +12,14 @@ export default function TabsLayout() {
         },
       }}
     >
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: "Home",
-          tabBarIcon: ({ color }) => (
-            <FontAwesome size={28} name="home" color={color} />
-          ),
-        }}
-      />
-      <Tabs.Screen
-        name="settings"
-        options={{
-          title: "Settings",
-          tabBarIcon: ({ color }) => (
-            <FontAwesome size={28} name="cog" color={color} />
-          ),
-        }}
-      />
-    </Tabs>
+      <NativeTabs.Trigger name="index" >
+        <Label>Home</Label>
+        <Icon sf="house.fill" color="#9b5de5" drawable="custom_android_drawable" />
+      </NativeTabs.Trigger>
+      <NativeTabs.Trigger name="settings">
+        <Icon sf="gear" color="#9b5de5" drawable="custom_settings_drawable" />
+        <Label>Settings</Label>
+      </NativeTabs.Trigger>
+    </NativeTabs>
   );
 }
