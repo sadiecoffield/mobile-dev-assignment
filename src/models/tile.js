e82d800e115e9eede3f20eb20203b0b79c84db29
sadiecoffield <sadiec05@yahoo.com>
2025-11-15 18:06:28 +0000

    Add default values to tile factory function

--- src/models/tile.js
+++ src/models/tile.js
@@ -1,8 +1,15 @@
 import * as Crypto from "expo-crypto";
 
 // Returns tile objects
-export function createTile({ text, zone = "", icon, photoUri = null }) {
+export function createTile({
+  text = "",
+  zone = "",
+  iconLibrary = "",
+  iconName = "",
+  iconColor = "",
+  photoUri = "",
+}) {
   const id = Crypto.randomUUID();
 
-  return { id, text, zone, icon };
+  return { id, text, zone, iconLibrary, iconName, iconColor, photoUri };
 }
